f1a1ef9d4668c50a0621053a86cb3af23b15adc0
Tyler Wilmoth <tyler@bittorrent.com>
2015-07-21 15:59:17 -0700

    Implement ITorrentEventObserver

--- torrentobserver.h
+++ torrentobserver.h
@@ -8,5 +8,13 @@ public:
         virtual void onTorrentAdded(const Argo::SHA1Hash &hash);
         virtual void onTorrentRemoved(const Argo::SHA1Hash &hash);
 };
-#endif // TORRENTOBSERVER_H
 
+class TorrentFileObserver : public Argo::ITorrentEventObserver {
+public:
+    virtual void onMetadataReceived() override;
+    virtual void onPieceCompleted(int piece) override;
+    virtual void onTorrentCompleted() override;
+    virtual void onTorrentReaped() override;
+};
+
+#endif // TORRENTOBSERVER_H
