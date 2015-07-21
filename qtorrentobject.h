a82728ff87ad46b59568cc2c1f39a4045b7dbea8
Francisco De La Cruz <francisco@bittorrent.com>
2015-07-21 15:30:08 -0700

    Added extra comments

--- qtorrentobject.h
+++ qtorrentobject.h
@@ -8,10 +8,13 @@ class QTorrentObject : public QObject
     Q_OBJECT
 public:
     explicit QTorrentObject(QObject *parent = 0);
+    // Setup some metadata
     Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
     Q_PROPERTY(int downloadedBytes READ downloadedBytes WRITE setDownloadedBytes NOTIFY downloadedBytesChanged)
+
     QTorrentObject(const QString& name, int dled);
 
+    // Setters
     void setName(const QString& name) {
         this->m_name = name;
         emit nameChanged(name);
@@ -21,12 +24,13 @@ public:
         emit downloadedBytesChanged(dledBytes);
     }
 
+    // Getters
     QString name() { return m_name; }
     int downloadedBytes() { return m_downloadedBytes; }
+
 private:
     QString m_name;
     int m_downloadedBytes;
-
 signals:
     void nameChanged(QString newName);
     void downloadedBytesChanged(int dled);
